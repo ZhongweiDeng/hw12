ce6aab0d8d9399ed322e72aaad3e40b4dca30e69
Betty <55335473+ZhongweiDeng@users.noreply.github.com>
2019-12-02 18:02:40 -0800

    Create log.js

---
+++ log.js
@@ -0,0 +1,46 @@
+class Log extends Rectangle {
+
+
+
+    constructor(x, y, width, height, speed) {
+
+        super(x, y, width, height);
+
+        this.speed = speed;
+
+    }
+
+
+
+    show(){
+
+        fill(142, 103, 81);
+
+        rect(this.x, this.y, this.width, this.height);
+
+    }
+
+
+
+    update(){
+
+       this.x += this.speed;
+
+
+
+       if(this.speed > 0 && this.x > width+grid){
+
+           this.x = - this.width - grid;
+
+       } else if(this.speed < 0 && this.x < -this.width-grid){
+
+            this.x = width + grid;
+
+       }
+
+    }
+
+
+
+
+}
